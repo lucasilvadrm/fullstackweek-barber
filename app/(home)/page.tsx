ece996fde45e3026d4c4dc84167ab2b3d4809015
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-30 11:29:02 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="py-6 px-5 flex flex-col gap-1">
+        <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
